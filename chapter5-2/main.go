a1ccf2f73fa6ec13899a657de2180698c152095f
aj.jammy <aj.jammy09@gmail.com>
2018-02-21 11:15:51 +0700

    Create fizzBuzz function

--- chapter5-2/main.go
+++ chapter5-2/main.go
@@ -1,17 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
 	for number := 1; number <= 100; number++ {
-		if number%15 == 0 {
-			fmt.Println(number, "FizzBuzz")
-		} else if number%3 == 0 {
-			fmt.Println(number, "Fizz")
-		} else if number%5 == 0 {
-			fmt.Println(number, "Buzz")
-		} else {
-			fmt.Println(number)
-		}
+		fmt.Println(number, fizzBuzz(number))
 	}
+}
+
+func fizzBuzz(number int) string {
+	if number%15 == 0 {
+		return "FizzBuzz"
+	} else if number%3 == 0 {
+		return "Fizz"
+	} else if number%5 == 0 {
+		return "Buzz"
+	}
+	return strconv.Itoa(number)
 }
