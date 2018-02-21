d536ec3f847f7e7dda5aa83925b701d4771a2dec
aj.jammy <aj.jammy09@gmail.com>
2018-02-21 10:33:09 +0700

    Add chapter9-1 struct type

---
+++ chapter9-1/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+type Circle struct {
+	x float64
+	y float64
+	r float64
+}
+
+func main() {
+	var c Circle
+	fmt.Printf(" c type: %T\n", c)
+	fmt.Println(c.x, c.y, c.r)
+
+	c1 := new(Circle)
+	fmt.Printf(" c type: %T\n", c1)
+	fmt.Println(c1.x, c1.y, c1.r)
+
+	c2 := Circle{x: 0, y: 0, r: 5}
+	fmt.Printf(" c type: %T\n", c2)
+	fmt.Println(c2.x, c2.y, c2.r)
+
+	c3 := NewCircle(1, 2, 3)
+	fmt.Printf(" c type: %T\n", c3)
+	fmt.Println(c3.x, c3.y, c3.r)
+}
+
+func NewCircle(x, y, r float64) *Circle {
+	return &Circle{x, y, r}
+}
