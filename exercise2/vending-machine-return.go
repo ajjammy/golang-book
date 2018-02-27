0b055152d8c00b253c8c383deeb12abdc2e5c2af
aj.jammy <aj.jammy09@gmail.com>
2018-02-27 10:01:54 +0700

    move items to VendingMachine struct

--- exercise2/vending-machine-return.go
+++ exercise2/vending-machine-return.go
@@ -5,6 +5,7 @@ import "fmt"
 type VendingMachine struct {
 	insertedMoney int
 	coins         map[string]int
+	items         map[string]int
 }
 
 func (m VendingMachine) InsertedMoney() int {
@@ -16,29 +17,37 @@ func (m *VendingMachine) InsertCoin(coin string) {
 }
 
 func (m *VendingMachine) SelectSD() string {
+	price := m.items["SD"]
+	change := m.insertedMoney - price
 	m.insertedMoney = 0
-	return "SD"
+	return "SD" + m.change(change)
 }
 
 func (m *VendingMachine) SelectCC() string {
-	price := 12
+	price := m.items["CC"]
 	change := m.insertedMoney - price
+	m.insertedMoney = 0
 	return "CC" + m.change(change)
 }
 
 func (m VendingMachine) change(c int) string {
+	if c == 0 {
+		return ""
+	}
 	return ", F, TW, O"
 }
 
 func main() {
 	var coins = map[string]int{"T": 10, "F": 5, "TW": 2, "O": 1}
+	var items = map[string]int{"SD": 18, "CC": 12, "DW": 7}
 
-	vm := VendingMachine{coins: coins}
+	vm := VendingMachine{coins: coins, items: items}
 	fmt.Println("Inserted Money:", vm.InsertedMoney())
 
 	vm.InsertCoin("T")
 	vm.InsertCoin("F")
 	vm.InsertCoin("TW")
+	vm.InsertCoin("O")
 	fmt.Println("Inserted Money:", vm.InsertedMoney())
 	can := vm.SelectSD() // SD
 	fmt.Println(can)
