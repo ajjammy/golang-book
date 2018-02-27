b486c52e0f14d57542286cbc9ac76e8cb7dbbfa6
aj.jammy <aj.jammy09@gmail.com>
2018-02-28 00:03:09 +0700

    Create vendinMachine

--- exercise2/vending-machine.go
+++ exercise2/vending-machine.go
@@ -2,120 +2,85 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
-type VendingMachine struct {
-	coins []string
-	items []string
-}
+var coins = map[string]int{"T": 10, "F": 5, "TW": 2, "O": 1}
+var items = map[string]int{"SD": 18, "CC": 12, "DW": 7}
 
 func main() {
-	coins := []string{}
-	items := []string{}
 	vm := NewVendingMachine(coins, items)
+	fmt.Println("Inserted Money:", vm.InsertedMoney())
 
-	//Buy SD(soft drink) with exact change
-	fmt.Println()
 	vm.InsertCoin("T")
 	vm.InsertCoin("F")
 	vm.InsertCoin("TW")
 	vm.InsertCoin("O")
-	fmt.Println("Inserted Money:", vm.GetInsertedMoney()) // 18
-	fmt.Println("Choose: Select SD")
-	can := vm.SelectSD()
-	fmt.Println("Return:", can) // SD
+	fmt.Println("Inserted Money:", vm.InsertedMoney())
+	can := vm.SelectSD() // SD
+	fmt.Println(can)
 
-	vm = NewVendingMachine(coins, items)
-
-	//Buy CC(canned coffee) without exact change
-	fmt.Println()
 	vm.InsertCoin("T")
 	vm.InsertCoin("T")
-	fmt.Println("Inserted Money:", vm.GetInsertedMoney()) // 20
-	fmt.Println("Choose: Select CC")
+	fmt.Println("Inserted Money:", vm.InsertedMoney())
 	can = vm.SelectCC()
-	fmt.Println("Return:", can) // CC, F, TW, O
+	fmt.Println(can) // CC, F, TW, O
 
-	vm = NewVendingMachine(coins, items)
-
-	// Start adding change but hit coin return
-	fmt.Println()
 	vm.InsertCoin("T")
 	vm.InsertCoin("T")
 	vm.InsertCoin("F")
-	fmt.Println("Inserted Money:", vm.GetInsertedMoney()) // 25
-	fmt.Println("Choose: Coin Return")
-	change := vm.CoinReturn()
-	fmt.Println("Return:", change) // T, T, F
+	fmt.Println("Inserted Money:", vm.InsertedMoney())
+	can = vm.CoinReturn()
+	fmt.Println(can) // T, T, F
 }
 
-func (vm *VendingMachine) CoinReturn() string {
-	vm.items = []string{}
-	return strings.Join(vm.coins, ", ")
+type VendingMachine struct {
+	insertedMoney int
+	coins         map[string]int
+	items         map[string]int
 }
 
-func (vm *VendingMachine) SelectCC() string {
-	vm.items = append(vm.items, "CC")
-	insertedMoney := vm.GetInsertedMoney()
-	priceCC := 12
-
-	balance := insertedMoney - priceCC
-	if balance > 0 {
-		return vm.GetSelectedItems() + ", " + getDisplayMoney(balance)
-	} else {
-		return vm.GetSelectedItems()
-	}
+func NewVendingMachine(coins, items map[string]int) *VendingMachine {
+	return &VendingMachine{coins: coins, items: items}
 }
 
-func (vm *VendingMachine) SelectSD() string {
-	vm.items = append(vm.items, "SD")
-	return vm.GetSelectedItems()
+func (m VendingMachine) InsertedMoney() int {
+	return m.insertedMoney
 }
 
-func (vm VendingMachine) GetSelectedItems() string {
-	return strings.Join(vm.items, ", ")
+func (m *VendingMachine) InsertCoin(coin string) {
+	m.insertedMoney += m.coins[coin]
 }
 
-func (vm VendingMachine) GetInsertedMoney() int {
-	coin := map[string]int{
-		"T":  10,
-		"F":  5,
-		"TW": 2,
-		"O":  1,
-	}
-
-	var sumCoinValue int
-	for _, value := range vm.coins {
-		sumCoinValue += coin[value]
-	}
-
-	return sumCoinValue
+func (m *VendingMachine) SelectSD() string {
+	price := m.items["SD"]
+	change := m.insertedMoney - price
+	m.insertedMoney = 0
+	return "SD" + m.change(change)
 }
 
-func getDisplayMoney(money int) string {
-	displayMoney := []string{}
-	coinsValue := []int{10, 5, 2, 1}
-	coinsDisplay := []string{"T", "F", "TW", "O"}
+func (m *VendingMachine) SelectCC() string {
+	price := m.items["CC"]
+	change := m.insertedMoney - price
+	m.insertedMoney = 0
+	return "CC" + m.change(change)
+}
 
-	balance := money
-
-	for index := 0; index < len(coinsValue); index++ {
-		if balance/coinsValue[index] > 0 {
-			for i := 0; i < (balance / coinsValue[index]); i++ {
-				displayMoney = append(displayMoney, coinsDisplay[index])
-			}
-			balance = balance % coinsValue[index]
+func (m VendingMachine) change(c int) string {
+	var str string
+	values := [...]int{10, 5, 2, 1}
+	coins := [...]string{"T", "F", "TW", "O"}
+	for i := 0; i < len(values); i++ {
+		if c >= values[i] {
+			str += ", " + coins[i]
+			c -= values[i]
+			i--
 		}
 	}
-
-	return strings.Join(displayMoney, ", ")
+	return str
 }
 
-func (vm *VendingMachine) InsertCoin(coin string) {
-	vm.coins = append(vm.coins, coin)
-}
-
-func NewVendingMachine(coins, items []string) *VendingMachine {
-	return &VendingMachine{coins, items}
+func (m *VendingMachine) CoinReturn() string {
+	coins := m.change(m.insertedMoney)
+	m.insertedMoney = 0
+	return coins[2:len(coins)]
 }
