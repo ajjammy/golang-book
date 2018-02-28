31a82ab1ae2d5f8aa5e5039529ecd2ab02281050
aj.jammy <aj.jammy09@gmail.com>
2018-02-28 10:34:13 +0700

    Modify use package form github.com

--- exercise/vending-machine.go
+++ exercise/vending-machine.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/ajjammy/vendingMachine"
 )
 
 var coins = map[string]int{"T": 10, "F": 5, "TW": 2, "O": 1}
 var items = map[string]int{"SD": 18, "CC": 12, "DW": 7}
 
 func main() {
-	vm := NewVendingMachine(coins, items)
+	vm := vendingMachine.NewVendingMachine(coins, items)
 	fmt.Println("Inserted Money:", vm.InsertedMoney())
 
 	vm.InsertCoin("T")
@@ -31,56 +33,4 @@ func main() {
 	fmt.Println("Inserted Money:", vm.InsertedMoney())
 	can = vm.CoinReturn()
 	fmt.Println(can) // T, T, F
-}
-
-type VendingMachine struct {
-	insertedMoney int
-	coins         map[string]int
-	items         map[string]int
-}
-
-func NewVendingMachine(coins, items map[string]int) *VendingMachine {
-	return &VendingMachine{coins: coins, items: items}
-}
-
-func (m VendingMachine) InsertedMoney() int {
-	return m.insertedMoney
-}
-
-func (m *VendingMachine) InsertCoin(coin string) {
-	m.insertedMoney += m.coins[coin]
-}
-
-func (m *VendingMachine) SelectSD() string {
-	price := m.items["SD"]
-	change := m.insertedMoney - price
-	m.insertedMoney = 0
-	return "SD" + m.change(change)
-}
-
-func (m *VendingMachine) SelectCC() string {
-	price := m.items["CC"]
-	change := m.insertedMoney - price
-	m.insertedMoney = 0
-	return "CC" + m.change(change)
-}
-
-func (m VendingMachine) change(c int) string {
-	var str string
-	values := [...]int{10, 5, 2, 1}
-	coins := [...]string{"T", "F", "TW", "O"}
-	for i := 0; i < len(values); i++ {
-		if c >= values[i] {
-			str += ", " + coins[i]
-			c -= values[i]
-			i--
-		}
-	}
-	return str
-}
-
-func (m *VendingMachine) CoinReturn() string {
-	coins := m.change(m.insertedMoney)
-	m.insertedMoney = 0
-	return coins[2:len(coins)]
 }
