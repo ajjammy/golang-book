55598eec9ac1cc46a7d4e9ca4ccd79c8bb9bf17c
aj.jammy <aj.jammy09@gmail.com>
2018-03-07 15:01:27 +0700

    Create function add

--- chapter12-1/main.go
+++ chapter12-1/main.go
@@ -5,9 +5,9 @@ import (
 )
 
 func main() {
-	var addVar func(int, int) int
-	addVar = func(a, b int) int {
-		return a + b
-	}
-	fmt.Println(addVar(2, 3))
+	fmt.Println(add(2, 3))
+}
+
+func add(a, b int) int {
+	return a + b
 }
