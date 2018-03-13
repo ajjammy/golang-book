5d12e79eedc466b0e63afe0e8b4dd920f6dbe944
aj.jammy <aj.jammy09@gmail.com>
2018-03-13 21:15:24 +0700

    Add chapter14-1 http

---
+++ chapter14-1/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc(
+		"/",
+		func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintln(w, "Hello, World!")
+		})
+
+	http.ListenAndServe(":3000", nil)
+}
