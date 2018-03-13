32df12560fea9ee8393077bcaf12167aa53e13ef
aj.jammy <aj.jammy09@gmail.com>
2018-03-13 21:30:43 +0700

    Use http HandleFunc

--- chapter14-1/main.go
+++ chapter14-1/main.go
@@ -6,11 +6,12 @@ import (
 )
 
 func main() {
-	http.HandleFunc(
-		"/",
-		func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprintln(w, "Hello, World!")
-		})
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { fmt.Fprintln(w, "Hello, World!") })
+
+	barHandler := func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello Bar!")
+	}
+	http.HandleFunc("/bar", barHandler)
 
 	http.ListenAndServe(":3000", nil)
 }
